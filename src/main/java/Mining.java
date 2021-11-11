aceb6e400971aaeaec1c7126b638cb08f1e24146
Javier <javiermoralesestevez@outlook.com>
2021-11-12 00:08:10 +0100

    find nonce

--- src/main/java/Mining.java
+++ src/main/java/Mining.java
@@ -37,7 +37,7 @@ public class Mining {
 
         //buscamos el verdadero nounce
         //nonce = Hashcat.launch("", "", "", "", total_process, total_process_parallel);
-        nonce = Searching.search(block, block.getDifficulty());
+        nonce = Searching.searchNonce(block, block.getDifficulty());
 
         block.setNonce(nonce);
         block.setBlockHash(Util.blockHash(block.show()));
--- src/main/java/Searching.java
+++ src/main/java/Searching.java
@@ -3,32 +3,45 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Searching {
 
-//    public static int nonceMAX = 4294967295;
+//    public static int nonceMAX = 4294967295; //máximo conceptual de un int
+    public static int nonceMAX = 999999999;
     public static int min = 0;
     public static int max = 100;
 
     //Búsqueda del nonce
-    public static String search(Block block, String target) {
+    public static String searchNonce(Block block, String target) {
         String nonce = "";
-        String caracteresHex = "0123456789abcdef";
+        int num = 0;
         String blockhash = Util.blockHash(block.show() + String.valueOf(0));
 
-        while (!blockhash.startsWith(target)) {
+        while (!blockhash.startsWith(target) && num < nonceMAX) {
             //Crear números aleaotorios sin posibilidad de repetirse
-            int randomNum1 = ThreadLocalRandom.current().nextInt(min, max + 1);
-            int randomNum2 = ThreadLocalRandom.current().nextInt(min, max + 1);
-            int randomNum3 = ThreadLocalRandom.current().nextInt(min, max + 1);
-            int randomNum4 = ThreadLocalRandom.current().nextInt(min, max + 1);
-            int randomNum5 = ThreadLocalRandom.current().nextInt(min, max + 1);
-            int randomNum6 = ThreadLocalRandom.current().nextInt(min, max + 1);
-            int randomNum7 = ThreadLocalRandom.current().nextInt(min, max + 1);
-            int randomNum8 = ThreadLocalRandom.current().nextInt(min, max + 1);
+            //nonce = charToHex();
 
-            //concantenar chars
-            nonce = String.valueOf(caracteresHex.charAt(randomNum1 % 16)) + String.valueOf(caracteresHex.charAt(randomNum2 % 16)) + String.valueOf(caracteresHex.charAt(randomNum3 % 16)) + String.valueOf(caracteresHex.charAt(randomNum4 % 16)) + String.valueOf(caracteresHex.charAt(randomNum5 % 16)) + String.valueOf(caracteresHex.charAt(randomNum6 % 16)) + String.valueOf(caracteresHex.charAt(randomNum7 % 16)) + String.valueOf(caracteresHex.charAt(randomNum8 % 16));
-            blockhash = Util.blockHash(block.show() + nonce);
+            //calcular blockhash
+            num++;
+            blockhash = Util.blockHash(block.show() + Util.numtoHex(num));
         }
         System.out.println("Nonce: " + nonce);
+        return nonce;
+    }
+
+
+    public static String charToHex(){
+        String nonce = "";
+        String caracteresHex = "0123456789abcdef";
+
+        //Crear números aleaotorios sin posibilidad de repetirse
+        int randomNum1 = ThreadLocalRandom.current().nextInt(min, max + 1);
+        int randomNum2 = ThreadLocalRandom.current().nextInt(min, max + 1);
+        int randomNum3 = ThreadLocalRandom.current().nextInt(min, max + 1);
+        int randomNum4 = ThreadLocalRandom.current().nextInt(min, max + 1);
+        int randomNum5 = ThreadLocalRandom.current().nextInt(min, max + 1);
+        int randomNum6 = ThreadLocalRandom.current().nextInt(min, max + 1);
+        int randomNum7 = ThreadLocalRandom.current().nextInt(min, max + 1);
+        int randomNum8 = ThreadLocalRandom.current().nextInt(min, max + 1);
+
+        nonce = String.valueOf(caracteresHex.charAt(randomNum1 % 16)) + String.valueOf(caracteresHex.charAt(randomNum2 % 16)) + String.valueOf(caracteresHex.charAt(randomNum3 % 16)) + String.valueOf(caracteresHex.charAt(randomNum4 % 16)) + String.valueOf(caracteresHex.charAt(randomNum5 % 16)) + String.valueOf(caracteresHex.charAt(randomNum6 % 16)) + String.valueOf(caracteresHex.charAt(randomNum7 % 16)) + String.valueOf(caracteresHex.charAt(randomNum8 % 16));
         return nonce;
     }
 
