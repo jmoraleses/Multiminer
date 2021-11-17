f41c5b92e67166a8185e7e78e1d3a81260bd7b71
Javier <javiermoralesestevez@outlook.com>
2021-11-17 11:41:42 +0100

    update

--- src/main/java/Model/Miner.java
+++ src/main/java/Model/Miner.java
@@ -10,9 +10,6 @@ import static Core.ScryptHelp.printByteArray;
 
 public class Miner {
 
-//    public static final String address = Util.ripemd160("03779c54c2c8aa4deb4f606953204f4c3b734ac51d30cc1152a98ebb603b010a1b");
-//    public static final String address = Util.ripemd160("niwMwv2fWek3HV5n4SLzEySZhxmUTAWgEs");
-
     public static final String seed = "seed3";
     public static final String publicKey = generatePublicKey(seed);
     public static final String privateKey = generatePrivateKey(seed);
